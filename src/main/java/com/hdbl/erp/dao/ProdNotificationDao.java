5d17d59dde884248e14e06584b5f2c820c2a9b0c
Ericgoodboy <776968336@qq.com>
2020-03-03 16:25:06 +0800

    修改了一些生产通知单

--- src/main/java/com/hdbl/erp/dao/ProdNotificationDao.java
+++ src/main/java/com/hdbl/erp/dao/ProdNotificationDao.java
@@ -2,15 +2,19 @@ package com.hdbl.erp.dao;
 
 import com.hdbl.erp.entity.ProdNotification;
 import com.hdbl.erp.entity.ProductBean;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+
+@Mapper
 public interface ProdNotificationDao {
-    public ArrayList<ProdNotification> select(@Param("args") HashMap<String,Object> args);
+    public ArrayList<ProdNotification> select(@Param("obj") HashMap<String,Object> args);
+    public int summary(@Param("obj") HashMap<String,Object> obj);
     public int update(@Param("condition") HashMap<String,Object> condition, @Param("data")HashMap<String,Object> data);
-    public int insert(List<ProdNotification> list);
+    public int insert(List<HashMap<String,Object>> list);
     public int delete(@Param("obj")HashMap<String,Object> obj);
 }
